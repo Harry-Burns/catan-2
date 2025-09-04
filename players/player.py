814466e586dcec3970216877cf8c249bc1790d08
Harry Burns <harry@familyburns.com>
2025-09-04 02:07:10 +0100

    New Player: RandomPlayerDistributed

--- players/player.py
+++ players/player.py
@@ -1,5 +1,6 @@
 import numpy as np
 from catan.state import GameState
+from catan.actions import unpack_action
 
 class Player:
     def __init__(self, player_id):
@@ -11,3 +12,10 @@ class Player:
 class RandomPlayer(Player):
     def decide(self, gs: GameState, playable_moves: np.ndarray):
         return playable_moves[np.random.randint(len(playable_moves))]
+    
+class RandomPlayerDistributed(Player):
+    def decide(self, gs: GameState, playable_moves: np.ndarray):
+        playable_actions = [unpack_action(a)[0] for a in playable_moves]
+        selected_action = np.random.choice(np.unique(playable_actions)) 
+        _playable_moves = [a for i,a in enumerate(playable_moves) if playable_actions[i] == selected_action]
+        return _playable_moves[np.random.randint(len(_playable_moves))]
